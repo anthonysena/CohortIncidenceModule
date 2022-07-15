f5f78ed5c95b7bb9d84e08873c97c9274463e27f
Anthony Sena <asena5@its.jnj.com>
2022-07-15 13:26:20 -0400

    Specify refId in buildOptions and Fix data model typos

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -7,7 +7,7 @@ createCohortIncidenceModuleSpecifications <- function(irDesign = NULL) {
   }
 
   specifications <- list(module = "CohortIncidenceModule",
-                         version = "0.0.2",
+                         version = "0.0.3",
                          remoteRepo = "github.com",
                          remoteUsername = "ohdsi",
                          settings = analysis)
